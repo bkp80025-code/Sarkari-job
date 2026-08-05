97eb388c68522fff9f1fc95c3182c35852e1f75e
bkp80025-code <bkp80025@gmail.com>
2026-08-05 21:37:27 +0530

    Update job posts and rendering logic

--- posts.js
+++ posts.js
@@ -1,35 +1,40 @@
 let posts = [
-
-{
-title:"RRB Group D Recruitment 2026",
-category:"Latest Jobs",
-link:"post.html"
-},
-{
-title:"IBPS Clerk 16th CRecruitment 2026",
-category:"Latest Jobs",
-link:"latest job/IBPS Clerk (CSA) 16th.html"
-},
-
-{
-title:"SSC GD Result 2026",
-category:"Result",
-link:"result.html"
-}
-
+  {
+    title: "IBPS Clerk 16th Online Form 2026",
+    category: "Latest Jobs",
+    link: "latest%20job/IBPS%20Clerk%20(CSA)%2016th.html"
+  },
+  {
+    title: "RRB Group D Recruitment 2026",
+    category: "Latest Jobs",
+    link: "post.html"
+  },
+  {
+    title: "SSC GD Exam Result 2026",
+    category: "Result",
+    link: "result.html"
+  },
+  {
+    title: "Railway Group D Admit Card 2026",
+    category: "Admit Card",
+    link: "admit-card.html"
+  }
 ];
 
-
-document.getElementById("postList").innerHTML =
-posts.map(function(post){
-
-return `
-<tr>
-<td>${post.title}</td>
-<td>
-<a href="${post.link}">View</a>
-</td>
-</tr>
-`;
-
-}).join("");
+// Auto Distribution System
+posts.forEach(function(post) {
+  let container = document.getElementById(post.category);
+  
+  if (container) {
+    container.innerHTML += `
+      <tr>
+        <td>
+          <a href="${post.link}" class="job-link">${post.title}</a>
+        </td>
+        <td style="text-align: right; width: 60px;">
+          <a href="${post.link}" class="btn-view">View</a>
+        </td>
+      </tr>
+    `;
+  }
+});
